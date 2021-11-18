a7e06613d69d730b8654df70c6be235d364ba4fc
Stan.exe <73562371+P4ladin@users.noreply.github.com>
2021-11-18 18:55:28 -0300

    deptController update

--- br.com.stanleyjuliomaciel/src/main/java/br/com/stanleyjuliomaciel/web/controllers/DepartamentoController.java
+++ br.com.stanleyjuliomaciel/src/main/java/br/com/stanleyjuliomaciel/web/controllers/DepartamentoController.java
@@ -1,12 +1,20 @@
 package br.com.stanleyjuliomaciel.web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.stanleyjuliomaciel.domain.Departamento;
+import br.com.stanleyjuliomaciel.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/departamento")
 public class DepartamentoController {
+	
+	@Autowired
+	private DepartamentoService service;
 	
 	@GetMapping("/cadastrar")
 	public String cadastrar( ) {
@@ -17,6 +25,12 @@ public class DepartamentoController {
 	@GetMapping("/listar")
 	public String listar( ) {
 		return "/departamento/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String Salvar(Departamento departamento) {
+		service.salvar(departamento);
+		return "redirect:/departamentos/cadastrar";
 	}
 
 }
